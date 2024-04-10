83f4a7c9f9c36adb44b777a8000464cc52be4c3e
cweddle999 <157506275+cweddle999@users.noreply.github.com>
2024-04-10 09:49:30 -0500

    added and fixed scripts

--- TURN_BASED_RACER/Assets/Scripts/BattleSystem.cs
+++ TURN_BASED_RACER/Assets/Scripts/BattleSystem.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BattleState {START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    public Text dialogueText;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
     public BattleState state;
     // Start is called before the first frame update
     void Start()
@@ -17,20 +33,83 @@ public class BattleSystem : MonoBehaviour
     
     IEnumerator SetUpBattle()
     {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "Racer " + enemyUnit.unitName + "has appeared!";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+        
         yield return new WaitForSeconds(2f);
         state = BattleState.PLAYERTURN;
         PlayerTurn();
     }
-
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+        yield return new WaitForSeconds(1f);
+    }
     IEnumerator PlayerAttack()
     {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "The drift was successful!";
         yield return new WaitForSeconds(2f);
         
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
 
+    }
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+        
+        yield return new WaitForSeconds(1f);
+        
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            dialogueText.text = "YOU HAVE WON THE DEADLY RACE";
+
+        }
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "Sadly, you have lost the race";
+        }
     }
     void PlayerTurn()
     {
-
+        dialogueText.text = "Choose an action:";
     }
 
     public void OnAttack()
@@ -40,5 +119,13 @@ public class BattleSystem : MonoBehaviour
             return;
         }
         StartCoroutine(PlayerAttack());
+    }
+    public void OnHeal()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+        StartCoroutine(PlayerHeal());
     }
 }
